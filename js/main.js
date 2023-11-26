c9b73dc7007409545e08078f5e4d240561fa3078
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-11-26 14:00:53 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const counters = document.querySelectorAll('.value');
+const speed = 600;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('sajid');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 1);
+        }else{
+          counter.innerText = value + ' ' + 'k';
+        }
+     
+   }
+   
+   animate();
+});
+
+
+
+
+
+
+
