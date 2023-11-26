a0bbc8e6f73498b027c1c96bf24d28804563d279
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-11-27 00:11:19 +0600

    complite desktop size

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 const counters = document.querySelectorAll('.value');
-const speed = 600;
+const speed = 900;
 
-counters.forEach( counter => {
-   const animate = () => {
-      const value = +counter.getAttribute('sajid');
-      const data = +counter.innerText;
-     
-      const time = value / speed;
-     if(data < value) {
-          counter.innerText = Math.ceil(data + time);
-          setTimeout(animate, 1);
-        }else{
-          counter.innerText = value + ' ' + 'k';
+counters.forEach(counter => {
+    const animate = () => {
+        const value = +counter.getAttribute('sajid');
+        const data = +counter.innerText;
+
+        const time = value / speed;
+        if (data < value) {
+            counter.innerText = Math.ceil(data + time);
+            setTimeout(animate, 1);
+        } else {
+            counter.innerText = value + ' ' + 'k';
         }
-     
-   }
-   
-   animate();
+    }
+
+    animate();
 });
 
 
